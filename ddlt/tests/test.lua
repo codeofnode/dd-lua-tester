479102382a30d9e0b1a57cbebad4f910508e4afc
Ramesh Kumar <codeofnode@gmail.com>
2019-07-16 19:41:22 +0530

    setup and teardown feature

--- ddlt/tests/test.lua
+++ ddlt/tests/test.lua
@@ -102,45 +102,76 @@ local function load_json(path)
   return nil
 end
 
+local function callTests(tests, notTc)
+  for count = 1, #tests do
+    local test = tests[count]
+    local function tc()
+      local currentContext = _G
+      if type(test["require"]) == "string" then
+        if test["require"] ~= "$global" then
+          currentContext = resolveVar(test['require'])
+        end
+      else
+        currentContext = extractors["_context"]
+      end
+      local func = resolveVar(test["request"]["method"])
+      local result = {}
+      if type(currentContext[func]) == "function" then
+        local params = makeList(deepResolve(test["request"]["params"]))
+        local ok,err = currentContext[func](currentContext,unpack(params))
+        result['output'] = ok
+        result['error'] = err
+        if type(test["extractors"]) == "table" then
+          for k,v in pairs(test["extractors"]) do
+            local nk = resolveVar(k)
+            if type(nk) == 'string' and hasVariable(nk) == false then
+              extractors[nk] = queryJson(result,v)
+            end
+          end
+        end
+        if type(test["assertions"]) == "table" then
+          for k,v in pairs(test["assertions"]) do
+            assert.are.same(deepResolve(v), queryJson(result,resolveVar(k)))
+          end
+        end
+      end
+    end
+    if notTc == nil and type(test['summary']) == 'string' then
+      it(test['summary'], tc)
+    else
+      tc()
+    end
+  end
+end
+
 local function forOneTS(tsName, patt)
   local tsData = load_json(path.normpath(path.join(DDLT_GLOBAL_ARGS["d"], tsName..patt)))
   extractors["_context"] = require(path.join(DDLT_GLOBAL_ARGS["d"], tsName))
-  describe(tsName, function()
-    for count = 1, #tsData["tests"] do
-      local test = tsData["tests"][count]
-      it(test['summary'], function()
-        local currentContext = _G
-        if type(test["require"]) == "string" then
-          if test["require"] ~= "$global" then
-            currentContext = resolveVar(test['require'])
-          end
-        else
-          currentContext = extractors["_context"]
-        end
-        local func = resolveVar(test["request"]["method"])
-        local result = {}
-        if type(currentContext[func]) == "function" then
-          local params = makeList(deepResolve(test["request"]["params"]))
-          local ok,err = currentContext[func](currentContext,unpack(params))
-          result['output'] = ok
-          result['error'] = err
-          if type(test["extractors"]) == "table" then
-            for k,v in pairs(test["extractors"]) do
-              local nk = resolveVar(k)
-              if type(nk) == 'string' and hasVariable(nk) == false then
-                extractors[nk] = queryJson(result,v)
-              end
-            end
-          end
-          if type(test["assertions"]) == "table" then
-            for k,v in pairs(test["assertions"]) do
-              assert.are.same(queryJson(result,resolveVar(k)), deepResolve(v))
-            end
-          end
-        end
-      end)
-    end
-  end)
+  if is_array(tsData['setup']) then
+    lazy_setup(function()
+      callTests(tsData['setup'], true)
+    end)
+  end
+  if is_array(tsData['teardown']) then
+    lazy_teardown(function()
+      callTests(tsData['teardown'], true)
+    end)
+  end
+  if is_array(tsData['before_each']) then
+    before_each(function()
+      callTests(tsData['before_each'], true)
+    end)
+  end
+  if is_array(tsData['after_each']) then
+    after_each(function()
+      callTests(tsData['after_each'], true)
+    end)
+  end
+  if is_array(tsData['tests']) then
+    describe(tsName, function()
+      callTests(tsData['tests'])
+    end)
+  end
 end
 
 for count = 1, #DDLT_GLOBAL_ARGS["root"] do
